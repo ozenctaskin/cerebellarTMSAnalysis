4387867966c580a431e32b70ee086837b0ad591a
ozenc taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2024-08-19 03:22:42 -0400

    Script for visual inspection of CBI trials

---
+++ cleanCBITrials.m
@@ -0,0 +1,34 @@
+function droppedTrials = cleanCBITrials(dataPath)
+
+% This function loops through CBI trials and asks whether they should be
+% kept or discarded. Saves a new data with the same file name that starts
+% with "clean_". Only supports single channel.
+
+% Load data
+[filepath, filename, extension] = fileparts(dataPath);
+data = load(dataPath, [filename, '_wave_data']);
+data = data.([filename, '_wave_data']);
+
+% Create an empty struct to save the indices of to-be-deleted trials 
+droppedTrials = [];
+
+% Loop through data, plot, and ask whether to keep or delete. Save the
+% indices of dropped values
+for ii = 1:size(data.values,3)
+    plot(data.values(:,1,ii))
+    decision = input(['Drop trial number ' num2str(ii) '/' num2str(size(data.values,3)) ' enter: y/n: \n'], 's');
+    if strcmp(decision, 'y')
+        droppedTrials = [droppedTrials, ii];
+    end
+end
+
+% Drop the bad trials  
+data.values(:,:,droppedTrials) = [];
+data.frameinfo(droppedTrials,:) = [];
+
+save(fullfile(filepath, ['clean_' filename], extension), data)
+
+end
+
+
+
